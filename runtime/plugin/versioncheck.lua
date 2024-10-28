00a5f4813866db3e7d3fa8bf1c392e7768f1e08b
C.D. MacEachern <craig.daniel.maceachern@gmail.com>
2024-10-28 14:01:42 -0400

    refactor: simplify logic
    We now have better vim.version() features since last time I tried this

--- runtime/plugin/versioncheck.lua
+++ runtime/plugin/versioncheck.lua
@@ -1,32 +1,22 @@
-if vim.g.versioncheck == false then
-  return
-end
+-- flag to skip running this module
+if vim.g.versioncheck == false then return end
+
+-- this plugin is only meant to run on prerelease builds 
+if not vim.version().prerelease then return end
 
 -- detect non-interactive startups
-if vim.tbl_contains(vim.v.argv, '-l') then
-  return
-end
+if vim.tbl_contains(vim.v.argv, '-l') then return end
 
--- startup with -i NONE means no shada file read or written, so bail
-if vim.tbl_contains(vim.v.argv, '-i NONE') then
-  return
-end
+-- we are not allowed read/write the shada file, which we need
+if vim.tbl_contains(vim.v.argv, '-i NONE') then return end
 
 -- missing '!' in shada option means no storing/reading of global vars
-if not vim.o.shada:match('!') then
-  return
-end
+if not vim.o.shada:match('!') then return end
 
-if vim.version().prerelease ~= true then
-  return
-end
-
-vim.g.versioncheck = {}
-
-local augroup = vim.api.nvim_create_augroup('versioncheck', {})
+local group = vim.api.nvim_create_augroup("versioncheck", {})
 vim.api.nvim_create_autocmd('CursorHold', {
-  group = augroup,
-  desc = 'Tell user about news.txt changes.',
+  group = group,
+  desc = 'Offer to show detected changes to the runtime news.txt file.',
   callback = function()
     require('versioncheck').check_for_news()
   end,
