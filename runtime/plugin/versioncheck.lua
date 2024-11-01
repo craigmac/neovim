035539989bbcaa23d50a63efc5195fda6dede6b2
C.D. MacEachern <craig.daniel.maceachern@gmail.com>
2024-11-01 17:10:14 -0400

    updates

--- runtime/plugin/versioncheck.lua
+++ runtime/plugin/versioncheck.lua
@@ -1,25 +1,45 @@
--- flag to skip running this module
-if vim.g.versioncheck == false then return end
+-- user flag to opt-out
+if not vim.g.versioncheck then
+  return
+end
 
 -- this plugin is only meant to run on prerelease builds 
-if not vim.version().prerelease then return end
+if not vim.version().prerelease then
+  return
+end
 
 -- detect non-interactive startups
-if vim.tbl_contains(vim.v.argv, '-l') then return end
+if vim.tbl_contains(vim.v.argv, '-l') then
+  return
+end
 
 -- we are not allowed read/write the shada file, which we need
-if vim.tbl_contains(vim.v.argv, '-i NONE') then return end
+if vim.tbl_contains(vim.v.argv, '-i NONE') then
+  return
+end
 
 -- missing '!' in shada option means no storing/reading of global vars
-if not vim.o.shada:match('!') then return end
+if not vim.o.shada:match('!') then
+  return
+end
 
-local group = vim.api.nvim_create_augroup("versioncheck", {})
+-- TODO: should this go in callback instead? need to figure out timing
+-- of when these are set
+if vim.g.vscode or vim.g.firenvim then
+  return
+end
+
+-- global to indicate that we've loaded if we've made it here
+vim.g.versioncheck = {}
+
+local augroup = vim.api.nvim_create_augroup("versioncheck", {})
+
 vim.api.nvim_create_autocmd('CursorHold', {
-  group = group,
+  group = augroup,
   desc = 'Offer to show detected changes to the runtime news.txt file.',
+  once = true,
+  nested = true,
   callback = function()
     require('versioncheck').check_for_news()
   end,
-  once = true,
-  nested = true,
 })
