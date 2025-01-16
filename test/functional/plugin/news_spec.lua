cf0a4c149251f0f545f8beae2eb69167e1b689be
C.D. MacEachern <craig.daniel.maceachern@gmail.com>
2025-01-16 16:18:28 -0500

    fix: move to chan info to detect non TUI clients

--- test/functional/plugin/news_spec.lua
+++ test/functional/plugin/news_spec.lua
@@ -5,13 +5,13 @@ local api = n.api
 local clear = n.clear
 local eq = t.eq
 local fn = n.fn
-local matches = t.matches
 
 describe('news_plugin:', function()
   it('loads', function()
-    -- `-u` would stop the news plugin from loading at all
-    -- `-i NONE` turns off shada, which this plugin requires to load
+    -- `-u NONE` stops the plugin from loading
+    -- `-i NONE` turns off shada, which this plugin requires
     clear({ args_rm = { '-u', '-i' } })
+    api.nvim_set_client_info('nvim-tui', {}, "remote", {}, {})
     eq(1, fn.exists('news_check'))
   end)
 
@@ -22,7 +22,7 @@ describe('news_plugin:', function()
       eq(false, api.nvim_get_var('news_check'))
     end)
 
-    it('nvim was started clean (--clean)', function()
+    it('nvim was started with --clean flag', function()
       clear({
         args = { '--clean' },
         args_rm = { '-u', '-i' }
@@ -30,37 +30,12 @@ describe('news_plugin:', function()
       eq(0, fn.exists('news_check'))
     end)
 
-    it('nvim was started by firenvim', function()
-      clear({
-        args = { '--cmd', 'let g:started_by_firenvim = v:true' },
-        args_rm = { '-u', '-i' }
-      })
-      eq(0, fn.exists('news_check'))
-    end)
-
-    it('nvim was started by vscode-neovim', function()
-      clear({
-        args = { '--cmd', 'let g:vscode = v:true' },
-        args_rm = { '-u', '-i' }
-      })
-      eq(0, fn.exists('news_check'))
-    end)
-
-    -- Q: Does this need to be tested? hacky workaround to get it to work
-    -- running in the test instance because --embed conflicts with -l
-    it('nvim was started as Lua interpreter with -l', function()
-      local p = n.spawn_wait({
-        args_rm = {
-          '--embed',
-          '-u',
-        },
-        args = {
-          '-l',
-          'test/functional/fixtures/startup-plugin-news.lua',
-        }
-      })
-      matches('nil', p:output())
-    end)
+    -- meaning: embedded and used by something like firenvim, vscode-neovim, etc.
+    -- it('nvim builtin nvim-tui UI client is not running', function()
+    --   clear({ args_rm = { '-u', '-i' } })
+    --   api.nvim_set_client_info('foobar', {}, "embedder", {}, {})
+    --   eq(0, fn.exists('news_check'))
+    -- end)
 
     it('shada is turned off', function()
       clear({
